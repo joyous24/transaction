f709fa6cbd4bcd126b02553181313e8bf1639e14
zhaoxiqing <zhaoxiqing@zhen.com>
2020-05-05 13:43:31 +0800

    新增netty

---
+++ src/main/java/com/zxq/transaction/netty/NettyClient.java
@@ -0,0 +1,11 @@
+package com.zxq.transaction.netty;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * netty客户端
+ * @author zhaoxiqing
+ */
+@Slf4j
+public class NettyClient {
+}
---
+++ src/main/java/com/zxq/transaction/netty/NettyServer.java
@@ -0,0 +1,17 @@
+package com.zxq.transaction.netty;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * netty服务端
+ *
+ * @author zhaoxiqing
+ */
+@Slf4j
+public class NettyServer {
+
+    public void initNetty() {
+        
+    }
+
+}
