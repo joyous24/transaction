ffdaaebc1a43c6de1c578c9a8d426623f8ddd854
zhaoxiqing <mascot417@126.com>
2020-05-11 10:08:35 +0800

    修缮netty

--- src/main/java/com/zxq/transaction/netty/NettyClient.java
+++ src/main/java/com/zxq/transaction/netty/NettyClient.java
@@ -1,11 +1,83 @@
 package com.zxq.transaction.netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
 
 /**
  * netty客户端
+ *
  * @author zhaoxiqing
  */
 @Slf4j
 public class NettyClient {
+    public static void main(String[] args) throws Exception {
+        String host = "127.0.0.1";
+        int port = 8056;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new TimeClientHandler());
+                }
+            });
+
+            ChannelFuture f = b.connect(host, port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    static class TimeClientHandler extends ChannelInboundHandlerAdapter {
+        private ByteBuf buf;
+
+        @Override
+        public void handlerAdded(ChannelHandlerContext ctx) {
+            buf = ctx.alloc().buffer(4);
+        }
+
+        @Override
+        public void handlerRemoved(ChannelHandlerContext ctx) {
+            buf.release();
+            buf = null;
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            ByteBuf m = (ByteBuf) msg;
+            buf.writeBytes(m);
+            m.release();
+
+            if (buf.readableBytes() >= 4) {
+                long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
+                System.out.println(new Date(currentTimeMillis));
+                ctx.close();
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
 }
--- src/main/java/com/zxq/transaction/netty/NettyServer.java
+++ src/main/java/com/zxq/transaction/netty/NettyServer.java
@@ -1,5 +1,17 @@
 package com.zxq.transaction.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -9,9 +21,67 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class NettyServer {
+    private int port;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
 
     public void initNetty() {
-        
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+    }
+
+
+    class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelActive(final ChannelHandlerContext ctx) {
+            final ByteBuf time = ctx.alloc().buffer(4);
+            time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+            final ChannelFuture f = ctx.writeAndFlush(time);
+            f.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) {
+                    assert f == future;
+                    ctx.close();
+                }
+            });
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8056;
+        new NettyServer(port).initNetty();
     }
 
 }
