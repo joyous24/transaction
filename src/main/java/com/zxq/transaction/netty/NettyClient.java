15ada87887c93b0ef78b0bfc1c39d97f8ad2863f
zhaoxiqing <zhaoxiqing@zhen.com>
2020-05-19 15:32:24 +0800

    实现netty长连接通信

--- src/main/java/com/zxq/transaction/netty/NettyClient.java
+++ src/main/java/com/zxq/transaction/netty/NettyClient.java
@@ -2,18 +2,22 @@ package com.zxq.transaction.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.InetAddress;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * netty客户端
@@ -22,62 +26,126 @@ import java.util.Date;
  */
 @Slf4j
 public class NettyClient {
-    public static void main(String[] args) throws Exception {
-        String host = "127.0.0.1";
-        int port = 8056;
+    private String host;
+    private int port;
+    private Channel channel;
+    private Bootstrap b = null;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+        init();
+    }
+
+    private void init() {
+        b = new Bootstrap();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+        b.group(workerGroup).option(ChannelOption.SO_KEEPALIVE, true)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,
+                                Unpooled.copiedBuffer(System.getProperty("line.separator").getBytes())));
+                        //字符串编码解码
+                        pipeline.addLast("decoder", new StringDecoder());
+                        pipeline.addLast("encoder", new StringEncoder());
+                        //心跳检测
+                        pipeline.addLast(new IdleStateHandler(0, 4, 0, TimeUnit.SECONDS));
+                        //客户端的逻辑
+                        pipeline.addLast("handler", new NettyClientHandler(NettyClient.this));
 
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(workerGroup);
-            b.channel(NioSocketChannel.class);
-            b.option(ChannelOption.SO_KEEPALIVE, true);
-            b.handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                public void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new TimeClientHandler());
+                    }
+                });
+    }
+
+    public void start() {
+        ChannelFuture f = b.connect(host, port);
+        //断线重连
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if (!channelFuture.isSuccess()) {
+                    final EventLoop loop = channelFuture.channel().eventLoop();
+                    loop.schedule(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("not connect service");
+                            start();
+                        }
+                    }, 1L, TimeUnit.SECONDS);
+                } else {
+                    channel = channelFuture.channel();
+                    System.out.println("connected");
                 }
-            });
-
-            ChannelFuture f = b.connect(host, port).sync();
-
-            f.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-        }
-    }
-
-    static class TimeClientHandler extends ChannelInboundHandlerAdapter {
-        private ByteBuf buf;
-
-        @Override
-        public void handlerAdded(ChannelHandlerContext ctx) {
-            buf = ctx.alloc().buffer(4);
-        }
-
-        @Override
-        public void handlerRemoved(ChannelHandlerContext ctx) {
-            buf.release();
-            buf = null;
-        }
-
-        @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) {
-            ByteBuf m = (ByteBuf) msg;
-            buf.writeBytes(m);
-            m.release();
-
-            if (buf.readableBytes() >= 4) {
-                long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
-                System.out.println(new Date(currentTimeMillis));
-                ctx.close();
             }
+        });
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    class NettyClientHandler extends SimpleChannelInboundHandler {
+        private NettyClient nettyClient;
+        private String tenantId;
+        private int attempts = 0;
+
+        public NettyClientHandler(NettyClient nettyClient) {
+            this.nettyClient = nettyClient;
         }
 
         @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-            cause.printStackTrace();
-            ctx.close();
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+            System.out.println("service send message" + o.toString());
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("output connected!");
+            attempts = 0;
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("offline");
+            //使用过程中断线重连
+            final EventLoop eventLoop = ctx.channel().eventLoop();
+            if (attempts < 12) {
+                attempts++;
+            }
+            int timeout = 2 << attempts;
+            eventLoop.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    nettyClient.start();
+                }
+            }, timeout, TimeUnit.SECONDS);
+            ctx.fireChannelInactive();
+        }
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent) {
+                IdleStateEvent event = (IdleStateEvent) evt;
+                if (event.state().equals(IdleState.READER_IDLE)) {
+                    System.out.println("READER_IDLE");
+                } else if (event.state().equals(IdleState.WRITER_IDLE)) {
+                    //发送心跳，保持长连接
+                    String s = "NettyClient" + System.getProperty("line.separator");
+                    ctx.channel().writeAndFlush(s);  //发送心跳成功
+                } else if (event.state().equals(IdleState.ALL_IDLE)) {
+                    System.out.println("ALL_IDLE");
+                }
+            }
+            super.userEventTriggered(ctx, evt);
         }
     }
+
+    public static void main(String[] args) {
+        NettyClient nettyClient = new NettyClient("127.0.0.1", 20803);
+        nettyClient.start();
+    }
+
 }
--- src/main/java/com/zxq/transaction/netty/NettyServer.java
+++ src/main/java/com/zxq/transaction/netty/NettyServer.java
@@ -2,17 +2,24 @@ package com.zxq.transaction.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * netty服务端
@@ -21,67 +28,166 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class NettyServer {
-    private int port;
+    private int port = 20803;
+    /**
+     * 维护设备在线的表
+     */
+    private Map<String, Integer> clientMap = new HashMap<>();
 
-    public NettyServer(int port) {
-        this.port = port;
+    /**
+     * 维护设备连接的map 用于推送消息
+     */
+    private Map<String, Channel> channelMap = new HashMap<>();
+
+
+    public synchronized void setClient(String name) {
+        this.clientMap.put(name, 1);
     }
 
-    public void initNetty() {
+    public synchronized void removeClient(String name) {
+        this.clientMap.remove(name);
+    }
+
+    /**
+     * 判断连接处里面是否有东西
+     *
+     * @return
+     */
+    public synchronized boolean getClientMapSize() {
+        return this.clientMap.size() > 0;
+    }
+
+
+    public synchronized void setChannel(String name, Channel channel) {
+        this.channelMap.put(name, channel);
+    }
+
+    public synchronized Map<String, Channel> getChannelMap() {
+        return this.channelMap;
+    }
+
+    /**
+     * 发送消息给下游设备
+     *
+     * @param msg
+     * @return
+     */
+    public boolean writeMsg(String msg) {
+        boolean errorFlag = false;
+        Map<String, Channel> channelMap = getChannelMap();
+        if (channelMap.size() == 0) {
+            return true;
+        }
+        Set<String> keySet = clientMap.keySet();
+        for (String key : keySet) {
+            try {
+                Channel channel = channelMap.get(key);
+                if (!channel.isActive()) {
+                    errorFlag = true;
+                    continue;
+                }
+                channel.writeAndFlush(msg + System.getProperty("line.separator"));
+            } catch (Exception e) {
+                errorFlag = true;
+            }
+        }
+        return errorFlag;
+    }
+
+    public void bind() {
+        System.out.println("service start successful");
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        //特殊分隔符
+                        pipeline.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,
+                                Unpooled.copiedBuffer(System.getProperty("line.separator").getBytes())));
+                        pipeline.addLast("decoder", new StringDecoder());
+                        pipeline.addLast("encoder", new StringEncoder());
+                        pipeline.addLast(new IdleStateHandler(30, 0, 0, TimeUnit.SECONDS));
+                        pipeline.addLast("handler", new NettyServerHandler(NettyServer.this));
+                    }
+                });
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new TimeServerHandler());
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-            ChannelFuture f = b.bind(port).sync();
+            ChannelFuture f = bootstrap.bind(port).sync();
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
-
     }
 
+    class NettyServerHandler extends SimpleChannelInboundHandler {
+        private int counter = 0;
+        private NettyServer nettyServer;
 
-    class TimeServerHandler extends ChannelInboundHandlerAdapter {
+        public NettyServerHandler(NettyServer nettyServer) {
+            this.nettyServer = nettyServer;
+        }
 
         @Override
-        public void channelActive(final ChannelHandlerContext ctx) {
-            final ByteBuf time = ctx.alloc().buffer(4);
-            time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+            System.out.println("client say" + o.toString());
+            //重置心跳次数
+            counter = 0;
+        }
 
-            final ChannelFuture f = ctx.writeAndFlush(time);
-            f.addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture future) {
-                    assert f == future;
-                    ctx.close();
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            String clientName = ctx.channel().remoteAddress().toString();
+            System.out.println("RemoteAddress:" + clientName + "active!");
+            nettyServer.setClient(clientName);
+            nettyServer.setChannel(clientName, ctx.channel());
+            super.channelActive(ctx);
+            counter = 0;
+        }
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent) {
+                IdleStateEvent event = (IdleStateEvent) evt;
+                if (event.state().equals(IdleState.READER_IDLE)) {
+                    //空闲4s后触发
+                    if (counter >= 10) {
+                        ctx.channel().close().sync();
+                        String clientName = ctx.channel().remoteAddress().toString();
+                        System.out.println("" + clientName + "offline");
+                        nettyServer.removeClient(clientName);
+                        //判断是否有在线的
+                        if (nettyServer.getClientMapSize()) {
+                            return;
+                        }
+                    } else {
+                        counter++;
+                        System.out.println("loss" + counter + "count HB");
+                    }
                 }
-            });
-        }
-
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-            cause.printStackTrace();
-            ctx.close();
+            }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        int port = 8056;
-        new NettyServer(port).initNetty();
+    public static void main(String[] args) {
+        final NettyServer nettyServer = new NettyServer();
+        new Thread() {
+            @Override
+            public void run() {
+                nettyServer.bind();
+            }
+        }.start();
+        Scanner scanner = new Scanner(System.in);
+        String msg = "";
+        while (!(msg = scanner.nextLine()).equals("exit")) {
+            System.out.println(nettyServer.writeMsg(msg));
+        }
     }
-
 }
