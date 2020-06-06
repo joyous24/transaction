a57ba282652cffd041cc8ae50569d4f7f1725559
zhaoxiqing <zhaoxiqing@zhen.com>
2020-06-06 16:06:00 +0800

    增加分布式锁

--- src/main/java/com/zxq/transaction/TransactionApplication.java
+++ src/main/java/com/zxq/transaction/TransactionApplication.java
@@ -3,13 +3,22 @@ package com.zxq.transaction;
 import com.zxq.transaction.netty.NettyClient;
 import com.zxq.transaction.util.SpringContextUtil;
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.api.RedissonRxClient;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 系统服务运行入口
@@ -21,6 +30,17 @@ import org.springframework.web.client.RestTemplate;
 @EnableTransactionManagement
 @MapperScan("com.zxq.transaction.mapper")
 public class TransactionApplication {
+
+    @Bean
+    public RedissonClient redisson() throws IOException {
+        Config config = new Config();
+        config.useClusterServers()
+                .addNodeAddress("redis://127.0.0.1:6379");
+
+        File file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "redisson.yaml");
+        config = Config.fromYAML(file);
+        return Redisson.create(config);
+    }
 
     @Bean
     public NettyClient nettyClient() {
--- src/main/java/com/zxq/transaction/controller/UserController.java
+++ src/main/java/com/zxq/transaction/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.zxq.transaction.controller;
 
-import com.zxq.transaction.model.User;
 import com.zxq.transaction.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,12 @@ public class UserController {
 
     private UserService userService;
 
+    private RedissonClient redissonClient;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, RedissonClient redissonClient) {
         this.userService = userService;
+        this.redissonClient = redissonClient;
     }
 
     @RequestMapping("/saveUser")
@@ -35,5 +40,32 @@ public class UserController {
         log.info("==============saveUser 222222=============");
         userService.save2();
         return true;
+    }
+
+    @RequestMapping("/clock")
+    private void clock() throws InterruptedException {
+        // RLock 继承了 java.util.concurrent.locks.Lock 接口
+        RLock lock = redissonClient.getLock("lock");
+
+        lock.lock();
+        System.out.println("lock acquired");
+
+        Thread t = new Thread(() -> {
+            RLock lock1 = redissonClient.getLock("lock");
+            lock1.lock();
+            System.out.println("lock acquired by thread");
+            lock1.unlock();
+            System.out.println("lock released by thread");
+        });
+
+        t.start();
+        t.join(1000);
+
+        lock.unlock();
+        System.out.println("lock released");
+
+        t.join();
+
+        redissonClient.shutdown();
     }
 }
